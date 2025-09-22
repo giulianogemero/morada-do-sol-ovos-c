4dd3db5a14a0956293caac551ca510429e34aa58
Giuliano Gemero <giulianogemero@github.com>
2025-09-22 17:13:32 +0000

    fix: vite base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { VitePWA } from 'vite-plugin-pwa'
+
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, PluginOption } from "vite";
@@ -10,7 +12,31 @@ const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 👈 ADD THIS LINE
   plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Morada do Sol Ovos",
+        short_name: "Ovos",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    }),
     react(),
     tailwindcss(),
     // DO NOT REMOVE
@@ -22,4 +48,4 @@ export default defineConfig({
       '@': resolve(projectRoot, 'src')
     }
   },
-});
+})
